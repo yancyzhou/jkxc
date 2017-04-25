dd3c0c3cbc1ae0d214fb84220dda07f0fe959d15
yancyzhou <python@vip.126.com>
2017-04-25 22:14:22 +0800

    创建数据库表结构

--- model/models.py
+++ model/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column
 from sqlalchemy.orm import relationship,backref
 from config import Base
+from datetime import datetime
 from sqlalchemy.dialects.mysql import BIGINT, BINARY, BIT, BLOB, BOOLEAN, CHAR, DATE,DATETIME, DECIMAL, DECIMAL, DOUBLE, ENUM, FLOAT, INTEGER, LONGBLOB, LONGTEXT, MEDIUMBLOB, MEDIUMINT, MEDIUMTEXT, NCHAR, NUMERIC, NVARCHAR, REAL, SET, SMALLINT, TEXT, TIME, TIMESTAMP, TINYBLOB, TINYINT, TINYTEXT, VARBINARY, VARCHAR, YEAR
 class User(Base):
     __tablename__ = 'jkxc_staff'
@@ -27,29 +28,34 @@ class User(Base):
 
 class Student(Base):
     __tablename__ = "jkxc_student"
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8'
+    }
     student_id = Column(BIGINT(11),primary_key=True)
     student_code = Column(VARCHAR(13))
     student_name = Column(VARCHAR(50))
-    varchar(50),
-    student_nickname
-    varchar(100),
-    student_password
-    varchar(50),
-    student_headpic
-    varchar(100),
-    student_wxcode
-    varchar(50),
-    student_id_number
-    varchar(20),
-    student_pic
-    varchar(255),
-    student_state
-    int(11),
-    student_packageuid
-    int(11),
-    student_create_time
-    datetime
-    DEFAULT
-    'CURRENT_TIMESTAMP',
-    student_schooluid
-    int(11)
+    student_nickname = Column(VARCHAR(100))
+    student_password = Column(VARCHAR(50))
+    student_headpic = Column(VARCHAR(100))
+    student_wxcode = Column(VARCHAR(50))
+    student_id_number = Column(VARCHAR(20))
+    student_pic = Column(VARCHAR(255))
+    student_state = Column(BIGINT(11))
+    student_packageuid = Column(INTEGER(11))
+    student_create_time = Column(DATETIME,default='CURRENT_TIMESTAMP')
+    student_schooluid = Column(INTEGER(11))
+
+    def __init__(self,student_code=None,student_name=None,student_nickname = None,student_password = None,student_headpic = None ,student_wxcode = '',student_id_number= None,student_pic = None,student_state = 0,student_packageuid = None,student_create_time = datetime.now(),student_schooluid = None):
+        self.student_code = student_code
+        self.student_name = student_name
+        self.student_nickname = student_nickname
+        self.student_password = student_password
+        self.student_headpic = student_headpic
+        self.student_wxcode = student_wxcode
+        self.student_id_number = student_id_number
+        self.student_pic = student_pic
+        self.student_state = student_state
+        self.student_packageuid = student_packageuid
+        self.student_create_time = student_create_time
+        self.student_schooluid = student_schooluid
