ea3858e4592c7ac526e6dd2f712fb928db607214
yancyzhou <python@vip.126.com>
2017-04-30 12:01:19 +0800

    微信统一下单接口

--- app/api.py
+++ app/api.py
@@ -5,7 +5,7 @@
 ======================
 '''
 from __future__ import division
-from tornado import gen,escape
+from tornado import gen,escape,httpclient
 from datetime import datetime,timedelta
 from Handler import BaseHandler,ApiHTTPError
 from auth import jwtauth
@@ -46,8 +46,21 @@ class WXBizDataCrypt:
 class GetUserinfo(BaseHandler):
 
     executor = ThreadPoolExecutor(8)
+
     @gen.coroutine
     def get(self, *args, **kwargs):
+        self.code = self.get_json_argument("code",None)
+        self.appId = 'wxad81631247e48b3e'
+        client = httpclient.AsyncHTTPClient()
+        url = "https://api.weixin.qq.com/sns/jscode2session"
+        data = {
+            "appid":self.appId,
+            "secret":"fd82d4c64dbfead96192e31df1146741",
+            "js_code":self.code,
+            "grant_type":"authorization_code"
+        }
+        response = yield client.fetch(url, method="POST", body=json.dumps(data))
+        self.on_response(response)
         result = yield self.getdata()
         rep = {}
         rep['data'] = result
@@ -55,12 +68,12 @@ class GetUserinfo(BaseHandler):
 
     @run_on_executor
     def getdata(self):
-        appId = 'wx4f4bc4dec97d474b'
+
         sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
         encryptedData = 'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew=='
         iv = 'r7BXXKkLb8qrSNn05n0qiA=='
 
-        pc = WXBizDataCrypt(appId, sessionKey)
+        pc = WXBizDataCrypt(self.appId, sessionKey)
 
         rep = pc.decryptData(encryptedData, iv)
         return rep
