2fd7b0e99c5857af99c0028bf5c62b903256b84c
yancyzhou <python@vip.126.com>
2017-05-02 16:42:31 +0800

    注释 函数

--- app/api.py
+++ app/api.py
@@ -15,7 +15,7 @@ from concurrent.futures import ThreadPoolExecutor
 from tornado.concurrent import run_on_executor
 from model.models import *
 import requests,base64,json
-
+from Crypto.Cipher import AES
 
 class PackageIndex(BaseHandler):
     executor = ThreadPoolExecutor(8)
@@ -42,29 +42,29 @@ class PackageIndex(BaseHandler):
 
 
 
-# class WXBizDataCrypt:
-#     from Crypto.Cipher import AES
-#     def __init__(self, appId, sessionKey):
-#         self.appId = appId
-#         self.sessionKey = sessionKey
-#
-#     def decryptData(self, encryptedData, iv):
-#         # base64 decode
-#         sessionKey = base64.b64decode(self.sessionKey)
-#         encryptedData = base64.b64decode(encryptedData)
-#         iv = base64.b64decode(iv)
-#
-#         cipher = AES.new(sessionKey, AES.MODE_CBC, iv)
-#
-#         decrypted = json.loads(self._unpad(cipher.decrypt(encryptedData)))
-#
-#         if decrypted['watermark']['appid'] != self.appId:
-#             raise Exception('Invalid Buffer')
-#
-#         return decrypted
-#
-#     def _unpad(self, s):
-#         return s[:-ord(s[len(s) - 1:])]
+class WXBizDataCrypt:
+
+    def __init__(self, appId, sessionKey):
+        self.appId = appId
+        self.sessionKey = sessionKey
+
+    def decryptData(self, encryptedData, iv):
+        # base64 decode
+        sessionKey = base64.b64decode(self.sessionKey)
+        encryptedData = base64.b64decode(encryptedData)
+        iv = base64.b64decode(iv)
+
+        cipher = AES.new(sessionKey, AES.MODE_CBC, iv)
+
+        decrypted = json.loads(self._unpad(cipher.decrypt(encryptedData)))
+
+        if decrypted['watermark']['appid'] != self.appId:
+            raise Exception('Invalid Buffer')
+
+        return decrypted
+
+    def _unpad(self, s):
+        return s[:-ord(s[len(s) - 1:])]
 
 
 class PackageDetail(BaseHandler):
