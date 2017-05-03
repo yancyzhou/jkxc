ad5929c08497544068f62724ac6e7b9c7a16e479
yancyzhou <python@vip.126.com>
2017-05-03 13:02:10 +0800

    生成验证码

--- app/sms.py
+++ app/sms.py
@@ -46,7 +46,7 @@ class SmsSenders(BaseHandler):
         result = single_sender.send_with_param("86", self.phoneNumber, templ_id, params, "", "", ext)
         rsp = json.loads(result)
         print time.strptime(rsp['ext'], "%Y-%m-%d %H:%M:%S")
-        smlog = self.SmLog(smlog_usercode=self.phoneNumber,smlog_message=smlog_message,smlog_createtime=time.strptime(rsp['ext'], "%Y-%m-%d %H:%M:%S"))
+        smlog = self.SmLog(smlog_usercode=self.phoneNumber,smlog_message=smlog_message,smlog_createtime=rsp['ext'])
         print smlog
         self.DbRead.add(smlog)
         self.DbRead.commit()
