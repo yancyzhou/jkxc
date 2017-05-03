704f2578ada5708efb4222a91184737b610ba0b7
yancyzhou <python@vip.126.com>
2017-05-03 17:18:13 +0800

    添加验证验证码的路由

--- utils.py
+++ utils.py
@@ -5,12 +5,12 @@ from Handler import NotFound
 Handlers = [
     # (r"/download/(.*)", Download),
     (r'/api/auth', AuthHandler),
-    (r'/newapi/packagedetail', PackageDetail),
-    (r'/newapi/package', PackageIndex),
-    (r'/newapi/studentcourseslist', StudentExamList),
-    (r'/newapi/studentcoursesindex', StudentExamindex),
+    (r'/api/packagedetail', PackageDetail),
+    (r'/api/package', PackageIndex),
+    (r'/api/studentcourseslist', StudentExamList),
+    (r'/api/studentcoursesindex', StudentExamindex),
     (r'/api/GetUserinfo', GetUserinfo),
-    (r'/sms/SmsSender', SmsSenders),
-    (r'/sms/validationcode', ValidationCode),
+    (r'/api/SmsSender', SmsSenders),
+    (r'/api/validationcode', ValidationCode),
     (r".*", NotFound),
 ]
