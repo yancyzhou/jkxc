73c2dc888aedd301e914cfa558bc2d9a4d903179
yancyzhou <python@vip.126.com>
2017-05-03 10:57:22 +0800

    生成验证码

--- app/sms.py
+++ app/sms.py
@@ -40,11 +40,29 @@ class SmsSenders(BaseHandler):
         appid = self.sdkappid
         appkey = self.appkey
         templ_id = 18108
-
+        code = self.generate_verification_code_v2()
         single_sender = SmsSender.SmsSingleSender(appid, appkey)
-        params = ["5678", "3"]
+        params = [code, "3"]
         result = single_sender.send_with_param("86", self.phoneNumber, templ_id, params, "", "", "")
         rsp = json.loads(result)
         self.writejson(json_decode(str(ApiHTTPError(**rsp))))
 
 
+
+    def generate_verification_code_v2(self):
+        import random
+        ''' 随机生成6位的验证码 '''
+        code_list = []
+        for i in range(2):
+            random_num = random.randint(0, 9) # 随机生成0-9的数字
+            # 利用random.randint()函数生成一个随机整数a，使得65<=a<=90
+            # 对应从“A”到“Z”的ASCII码
+            a = random.randint(65, 90)
+            b = random.randint(97, 122)
+            random_uppercase_letter = chr(a)
+            random_lowercase_letter = chr(b)
+            code_list.append(str(random_num))
+            code_list.append(random_uppercase_letter)
+            code_list.append(random_lowercase_letter)
+        verification_code = ''.join(code_list)
+        return verification_code
