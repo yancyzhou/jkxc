64944b37fb2dbec0d3e26e67ca1977649763a52e
yancyzhou <python@vip.126.com>
2017-05-10 22:40:47 +0800

    添加返回值

--- app/pay.py
+++ app/pay.py
@@ -33,7 +33,7 @@ class SetOrder(BaseHandler):
         attachvalue = self.attachvalue
         mch_idvalue = "1467218302"  # mch_id
         nonce_strvalue = self.GetRandomStr
-        bodyvalue = self.body
+        bodyvalue = self.body.encode('utf-8')
         out_trade_novalue = self.id
         total_feevalue = self.total_fee  # 价格
         spbill_create_ipvalue = "120.210.166.7"
@@ -85,7 +85,7 @@ class SetOrder(BaseHandler):
         self.AppID = "wxad81631247e48b3e"
         self.id  = "JIKEXUECHE"+str(time.time()).replace(".","")+str(random.randint(10,100))
         self.attachvalue = "JKXC"
-        self.body = self.get_json_argument("body",None).encode('utf-8')
+        self.body = self.get_json_argument("body",None)
         self.total_fee = self.get_json_argument("total_fee",1000)
         self.openid = self.get_json_argument("openid",None)
         self.key = "jike712YMiinoo736Rexhu1217Nan909"
