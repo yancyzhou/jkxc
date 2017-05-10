e0a980b4d097c9d187c5354804e4a09f4f40e093
yancyzhou <python@vip.126.com>
2017-05-10 23:00:08 +0800

    添加返回值

--- app/pay.py
+++ app/pay.py
@@ -44,7 +44,7 @@ class SetOrder(BaseHandler):
         formatstr = 'appid=%s&attach=%s&body=%s&mch_id=%s&nonce_str=%s&notify_url=%s&openid=%s&out_trade_no=%s&spbill_create_ip=%s&total_fee=%s&trade_type=%s&key=%s' % (
                     appidvalue, attachvalue,bodyvalue,mch_idvalue,nonce_strvalue,notify_urlvalue,openidvalue,out_trade_novalue, spbill_create_ipvalue, total_feevalue,
                     trade_typevalue, key)
-
+        print formatstr
         signvalue = self.set_md5(formatstr)
 
         xmlstart = "<xml>\r\n"
