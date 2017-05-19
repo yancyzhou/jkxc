820285f53193bb1bf42e49f3453b4f1adcc23a6e
yancyzhou <python@vip.126.com>
2017-05-19 11:20:33 +0800

    commit

--- admin/__init__.py
+++
@@ -1,3 +0,0 @@
-from admin.index import *
-from admin.admini import *
-from admin.menu import *
--- app/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.user import *
-from app.api import *
-from app.sms import *
-from app.newapi import *
-from app.pay import *
