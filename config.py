7c2b451de5a14366471682ebb5631455083da36e
yancyzhou <python@vip.126.com>
2017-08-01 10:42:13 +0800

    添加修改错题本

--- config.py
+++ config.py
@@ -11,20 +11,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from dbclient import DBClient
 
-
 DBClients = DBClient()
-
 ServersPort = 8005
-# DB = 'mongodb://%s:%s@%s:%s/' % (DBClients.db.MongoDB.Auth.User,
-#                                  DBClients.db.MongoDB.Auth.Passwd, DBClients.db.MongoDB.Host, DBClients.db.MongoDB.Port)
-# Collections = DBClients.db.MongoDB.Collections
-# Settings = dict(
-#     template_path=os.path.join(os.path.dirname(__file__), "templates"),
-#     static_path=os.path.join(os.path.dirname(__file__), "static"),
-#     ui_modules={},
-#     debug=True,
-# )
-
 Base = declarative_base()
 EngineRead = create_engine('mysql://%s:%s@%s:%d/%s?charset=utf8' %
                        (DBClients.db.MySQLREAD.User, DBClients.db.MySQLREAD.Passwd,
